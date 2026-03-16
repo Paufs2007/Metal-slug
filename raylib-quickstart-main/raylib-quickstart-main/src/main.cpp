d88ec9e8b41d85588e90c0eb1a321803d107446c
Paufs2007 <pau.fornons@estudiantat.upc.edu>
2026-03-16 09:51:26 +0100

    animacions funcionen

--- raylib-quickstart-main/raylib-quickstart-main/src/main.cpp
+++ raylib-quickstart-main/raylib-quickstart-main/src/main.cpp
@@ -8,6 +8,8 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "raylib.h"
 #include "resource_dir.h"// utility header for SearchAndSetResourceDir
+#define MAX_FRAME_SPEED     15
+#define MIN_FRAME_SPEED      1
 class player
 {
 public:
@@ -27,17 +29,36 @@ int main()
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 	// Create the window and OpenGL context
+	const int screenWidth = 1920;
+	const int screenHeight = 1080;
 	InitWindow(1920, 1080, "Metal Slug");
 	ToggleFullscreen();
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 	// Load a texture from the resources directory
-	Texture p1 = LoadTexture("p1.png");
+	Texture p1 = LoadTexture("treure fons.png");
 	// game loop
-	player p = {0,0,0,0};
+	player p = {0,0,0,0, true};
+	Vector2 position = { p.x, p.y };
+	Rectangle frameRec = { p.x, p.y, (float)p1.width / 9, (float)p1.height };
+	int currentFrame = 0;
+	int framesCounter = 0;
+	int framesSpeed = 3;            // Number of spritesheet frames shown by second
 
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
+		framesCounter++;
+
+		if (framesCounter >= (60 / framesSpeed))
+		{
+			framesCounter = 0;
+			currentFrame++;
+
+			if (currentFrame > 9) currentFrame = 0; // el primer numero ha de ser el mateix que les divisions de la sprite sheet
+
+			frameRec.x = (float)currentFrame * (float)p1.width / 9; //numero divisons sprite sheet.
+		}
+		
 		p.x += p.vx;
 		p.y -= p.vy;
 		
@@ -66,22 +87,19 @@ int main()
 		{
 			p.jump();
 		}
-		
-
 
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(WHITE);
 
-		DrawRectangle(0, 775, 1920, 1000, BLACK);
-
-		// draw some text using the default font
-		DrawText("sol", 200, 200, 200, RED);
+		position.x = p.x;
+		position.y = p.y;
 
 		// draw our texture to the screen
-		DrawTexture(p1, p.x, p.y, WHITE);
+		DrawTexture(p1, 200, 150, WHITE);
 
+		DrawTextureRec(p1, frameRec, position, WHITE);  // Draw part of the texture
 
 		EndDrawing();// end the frame and get ready for the next one  (display frame, poll input, etc...)
 	}
